99840b0636fa80ffb2cc0afe8edb6548d35f6ebd
ThanhAnNguyen2014 <kieuquanh94@gmail.com>
2016-11-28 17:00:55 +0700

    update router Thanh An

--- src/ProjectShopTLCNCore/wwwroot/app/app.module.js
+++ src/ProjectShopTLCNCore/wwwroot/app/app.module.js
@@ -10,30 +10,45 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
+var router_1 = require('@angular/router');
 var app_component_1 = require('./app.component');
-var slide_component_1 = require('./Slide/slide.component');
 var header_component_1 = require('./Header/header.component');
 var footer_component_1 = require('./Footer/footer.component');
 var contact_component_1 = require('./contact/contact.component');
+//import { SlideComponent } from './Slide/slide.component';
 var service_component_1 = require('./service/service.component');
 var product_component_1 = require('./product/product.component');
 var about_component_1 = require('./about/about.component');
 var gallery_component_1 = require('./gallery/gallery.component');
 var cart_component_1 = require('./Cart/cart.component');
+var categories_component_1 = require('./categories/categories.component');
+//import { PreviewComponent } from './Preview/preview.component';
+var appRoutes = [
+    //{ path: 'product', component: ProductComponent },
+    { path: 'contact', component: contact_component_1.ContactComponent },
+    { path: 'service', component: service_component_1.ServiceComponent },
+    { path: 'about', component: about_component_1.AboutComponent },
+    { path: 'gallery', component: gallery_component_1.GalleryComponent },
+    { path: 'product', component: product_component_1.ProductComponent },
+    { path: 'categories', component: categories_component_1.CategoriesComponent },
+];
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
             imports: [
-                platform_browser_1.BrowserModule
+                platform_browser_1.BrowserModule,
+                router_1.RouterModule.forRoot(appRoutes)
             ],
             declarations: [
                 app_component_1.AppComponent,
-                slide_component_1.SlideComponent,
                 header_component_1.HeaderComponent,
+                //SlideComponent,
                 product_component_1.ProductComponent,
+                //PreviewComponent,
                 service_component_1.ServiceComponent,
+                categories_component_1.CategoriesComponent,
                 about_component_1.AboutComponent,
                 contact_component_1.ContactComponent,
                 gallery_component_1.GalleryComponent,
